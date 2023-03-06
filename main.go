5e35f999abafe3f01d7d9ab57ec97de86d6f0e38
red <33436708+b1tg@users.noreply.github.com>
2023-03-06 18:37:28 +0800

    trig ci

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	head := color.New(color.FgHiBlue).Add(color.Bold).SprintFunc()
 	note := color.New(color.FgGreen).SprintFunc()
 	log.Println(head("项目地址：github.com/zhanghanyun/backtrace"))
-	log.Println(note("正在测试三网回程路由..."))
+	log.Println(note("正在测试三网回程路由......"))
 
 	for i := range rIp {
 		go trace(c, i)
